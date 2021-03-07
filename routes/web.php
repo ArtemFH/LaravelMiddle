d030a13581cdd15fdc7ef83539833160025142a7
ArtemFH <artembereza1@mail.ru>
2021-03-08 01:02:41 +0700

    Middle

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,22 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('layouts.base');
+})->name('base');
 
 Route::name('user.')->group(function () {
-
-    Route::view('/private', 'private')->middleware('auth')->name('private');
-
-    Route::get('/logout', [\App\Http\Controllers\LogoutController::class, 'logout'])->name('logout');
+    Route::get('/registration', [\App\Http\Controllers\RegisterController::class, 'checkAvailability'])->name('registration');
 
     Route::get('/login', [\App\Http\Controllers\LoginController::class, 'checkAvailability'])->name('login');
 
-    Route::get('/registration', [\App\Http\Controllers\RegisterController::class, 'checkAvailability'])->name('registration');
+    Route::get('/logout', [\App\Http\Controllers\LogoutController::class, 'logout'])->name('logout');
+
+    Route::view('/private', 'private')->middleware('auth')->name('private');
 
     Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
 
