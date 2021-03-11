308c2ea09ea6bbbf6ef99e95e0b2cf7b80285ea4
ArtemFH <artembereza1@mail.ru>
2021-03-12 03:27:23 +0700

    laptop

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'username' => 'Artem',
+                'password' => '$2y$12$sDMLTH4SPGUP88qTXV20zOVJbVVPvKy03jEs8FcJWe2QXYFIG2MA2',
+                'email' => 'artembereza2@mail.ru',
+                'role_id' => '1',
+                'like_nomination_id' => '30'
+            ]
+        ];
+        DB::table('users')->insert($users);
+    }
+}
