87f224d851fb7a06f14033ecd45f8ec3d11fea4f
artem <artembereza1@mail.ru>
2021-03-19 22:16:49 +0700

    Middle

---
+++ app/Http/Controllers/RequestHardwareController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hardware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RequestHardwareController extends Controller
+{
+    public function requestHardware(Request $request)
+    {
+
+        $validateFields = $request->validate([
+            'CPU' => 'required',
+            'GPU' => 'required',
+            'RAM' => 'required',
+            'PSU' => 'required',
+            'storage' => 'required',
+            'motherboard' => 'required'
+        ]);
+
+        Hardware::create($validateFields);
+
+        return redirect(route('user.requestHardware'))->withErrors([
+            'formError' => 'Error'
+        ]);
+    }
+
+    public function requestHardwareAvailability()
+    {
+        $data = array(
+            'title' => 'Hardware'
+        );
+
+        return view('requires.requestHardware')->with($data);
+    }
+}
