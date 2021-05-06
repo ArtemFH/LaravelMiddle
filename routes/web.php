b32d1a71851da9a8f89c9871ec235c60b1158d28
artem <artembereza1@mail.ru>
2021-05-06 07:13:08 +0700

    Middle

--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,33 @@ Route::name('user.')->group(function () {
     Route::get('/deleteHardware', [\App\Http\Controllers\RequestHardwareController::class, 'deleteHardware'])->name('deleteHardware');
 });
 
+//Route::name('user.')->group(function () {
+//    Route::get('/registration', [\App\Http\Controllers\UserController::class, 'registrationAvailability'])->name('registration');
+//
+//    Route::post('/registration', [\App\Http\Controllers\UserController::class, 'createUser']);
+//
+//    Route::get('/login', [\App\Http\Controllers\UserController::class, 'loginAvailability'])->name('login');
+//
+//    Route::post('/login', [\App\Http\Controllers\UserController::class, 'login']);
+//
+//    Route::get('/logout', [\App\Http\Controllers\UserController::class, 'logout'])->name('logout');
+//
+//    Route::get('/profile', [\App\Http\Controllers\UserController::class, 'profileView'])->middleware('auth')->name('profile');
+//
+//    Route::get('/user/{indexUser}', [\App\Http\Controllers\UserController::class, 'profileUser'])->middleware('auth')->name('profileView');
+//
+//
+//    Route::post('/requestBenchmark', [\App\Http\Controllers\RequestHardwareController::class, 'requestBenchmark']);
+//
+//    Route::get('/requestBenchmark', [\App\Http\Controllers\RequestHardwareController::class, 'requestBenchmarkAvailability'])->name('requestBenchmark');
+//
+//    Route::post('/requestHardware', [\App\Http\Controllers\RequestHardwareController::class, 'requestHardware']);
+//
+//    Route::get('/requestHardware', [\App\Http\Controllers\RequestHardwareController::class, 'requestHardwareAvailability'])->name('requestHardware');
+//
+//    Route::post('/updateHardware', [\App\Http\Controllers\RequestHardwareController::class, 'updateHardware']);
+//
+//    Route::get('/updateHardware', [\App\Http\Controllers\RequestHardwareController::class, 'updateHardwareAvailability'])->name('updateHardware');
+//
+//    Route::get('/deleteHardware', [\App\Http\Controllers\RequestHardwareController::class, 'deleteHardware'])->name('deleteHardware');
+//});
